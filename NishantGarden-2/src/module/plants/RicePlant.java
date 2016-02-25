ba157e1e11102be7c66773e37f440e59132053e2
Nishant Phatangare <nishant1914@gmail.com>
2016-02-24 18:45:06 -0800

    different classes for plants added in plant module

---
+++ NishantGarden-2/src/module/plants/RicePlant.java
@@ -0,0 +1,20 @@
+/**
+ * COEN 275 OOAD - Individual Project
+ * Automated Gardening System
+ * 
+ * By Nishant Phatangare (W1108022)
+ */
+
+/*
+ * --About this <file>--
+ * 
+ */
+package module.plants;
+
+/**
+ * @author nishant
+ *
+ */
+public class RicePlant extends Plants {
+
+}
